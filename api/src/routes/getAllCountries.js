af4c5423a7df8dc9e84914a534287e82f9fbdb51
Lereysis <lereysisquezada@gmail.com>
2023-01-03 22:12:33 -0500

    error control

--- api/src/routes/getAllCountries.js
+++ api/src/routes/getAllCountries.js
@@ -4,15 +4,20 @@ const getCountries = require("../services/getCountries")
 const router = Router();
 
 router.get("/", async (req, res) => {
-    const {name} = req.query;
-    let countriesTotal = await getCountries();
-    if(name){
-     let countryName = await countriesTotal.filter( c=> c.name.toLowerCase().includes(name.toLocaleLowerCase()))
-     countryName.length ? 
-     res.status(200).send(countryName) :
-     res.status(404).send('No se encontró el país')
-    } else {
-     res.status(200).send(countriesTotal)
+    try {
+        const {name} = req.query;
+        let countriesTotal = await getCountries();
+        if(name){
+         let countryName = await countriesTotal.filter( c=> c.name.toLowerCase().includes(name.toLowerCase()))
+         countryName.length ? 
+         res.status(200).send(countryName) :
+         res.status(404).send('No se encontró el país')
+        } else {
+         res.status(200).send(countriesTotal)
+        } 
+    } catch (error) {
+        res.status(400).json({error: error.message})
+        next(error)
     }
  });
 
